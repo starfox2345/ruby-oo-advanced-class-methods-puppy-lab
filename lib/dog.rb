3dc2d8dc26e735207c2af2f8b1c1e9f9cfa5d11f
yatsun <yatsunlam25@gmail.com>
2021-03-17 19:21:02 -0400

    commit tree 419bce7bf174cea6d7b48626b6e212d30a6be768

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    attr_accessor :name
+    # attr_reader :name
+
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        # @@all << self
+        # self.save
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name }
+    end
+
+    def save
+        # if @@all == self
+            @@all << self
+        # end
+    end
+end
